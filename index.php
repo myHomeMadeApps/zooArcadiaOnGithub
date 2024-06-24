599b80d5be5c65460f883b5c37360249d9e7179c
myHomeMadeApps <freddevsoft@gmail.com>
2024-06-24 20:17:31 +0200

    commit de 2017

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
   header("Location:../Front/home-page.php"); 
-  //one useless change to test commit and push on github time1729
+  //one useless change to test commit and push on github time2006
 ?>
