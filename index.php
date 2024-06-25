c04efd4d82b3aeb8db734308ea563c1c2fff8b9c
myHomeMadeApps <freddevsoft@gmail.com>
2024-06-25 11:37:51 +0200

    commit du 25 à 1138

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
   header("Location:../Front/home-page.php"); 
-  //one useless change to test commit and push on github time2006
+  //one useless change to test commit and push on github time2048
 ?>
