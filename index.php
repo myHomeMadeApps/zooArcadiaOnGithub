6fa477109b970cbbbb242d1873a7b56dec4401d6
myHomeMadeApps <freddevsoft@gmail.com>
2024-07-19 18:11:22 +0200

    ours1907

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
   header("Location:../Front/home-page.php"); 
-  //1007one useless change to test commit and push on github time2048
+  //1007one useless change to test commit and push on github 19071810
 ?>
